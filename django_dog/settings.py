fed2ddfc2c7a2e049abcb52140821e0a7d52c005
jtother <juanramonquiroz15@gmail.com>
2020-06-12 22:55:40 -0500

    added heroku app to settings.py

--- django_dog/settings.py
+++ django_dog/settings.py
@@ -87,7 +87,9 @@ DATABASES = {
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:8000',
     'http://localhost:3000',
+    'http://rocky-wave-36222.herokuapp.com',
 ]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
