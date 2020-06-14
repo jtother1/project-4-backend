728ced83aca0d2b8465aa4b81f4c67c6bec1466e
jtother <juanramonquiroz15@gmail.com>
2020-06-13 23:56:21 -0500

    pipenv lock commit

--- django_dog/settings.py
+++ django_dog/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('users/', views.UserList.as_view(), name='user_list'),
-    path('user/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
+    path('users/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
 ]
